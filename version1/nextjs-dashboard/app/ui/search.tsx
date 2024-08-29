39615ea5aaa02d22660b5ef9f9d0a1bcb5f401e8
siddhidhamnaskar <siddhidhamnaskar64@gmail.com>
2024-08-30 01:06:54 +0530

    pagination added in invoices

--- version1/nextjs-dashboard/app/ui/search.tsx
+++ version1/nextjs-dashboard/app/ui/search.tsx
@@ -1,12 +1,13 @@
 'use client';
 import { useSearchParams ,usePathname, useRouter } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname=usePathname();
   const { replace }=useRouter();
-  function handleSearch(term: string) {
+  const handleSearch=useDebouncedCallback((term: string)=> {
     const params = new URLSearchParams(searchParams);
     console.log(term);
     if (term) {
@@ -15,7 +16,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`)
-  }
+  },300)
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
