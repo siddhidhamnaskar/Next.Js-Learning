1447b4bf6fbb41b8ee137f48a268749b2356bfb1
siddhidhamnaskar <siddhidhamnaskar64@gmail.com>
2024-09-02 00:44:07 +0530

    handling error in invoices implemented

--- version1/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ version1/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -6,6 +6,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
 import { Suspense } from 'react';
+
  
 export default async function Page({
   searchParams,
